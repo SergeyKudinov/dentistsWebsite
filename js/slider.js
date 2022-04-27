21c7fe83e1fdbbd64681c8c705fa72e2b6469b7c
Sergey Kudinov <serezha.kudinov.1995@mail.ru>
2022-04-27 14:40:12 +0300

    add slider

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+/* const swiper = new Swiper('.first-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 40,
+    loop: true,
+    pagination: {
+      el: '.first-swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    centeredSlides: true,
+    grabCursor: true,
+    initialSlide: 1,
+
+  }); */
