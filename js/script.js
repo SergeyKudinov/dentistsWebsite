f0d463d8bf125cf6d2d1568e571ccd855eabd66e
Sergey Kudinov <serezha.kudinov.1995@mail.ru>
2022-05-12 18:01:01 +0300

    nav-accordeon ready

--- js/script.js
+++ js/script.js
@@ -14,3 +14,23 @@ jQuery(document).ready(function ($) {
         j.preventDefault();
     });
 });
+
+
+function openCity(evt, cityName) {
+    let i, tabcontent, tablinks;
+    // Получить все элементы с class="tabcontent" и скрыть их
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Получить все элементы с class="tablinks" и снять класс "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Показать на текущей вкладке, и добавить класс "active" по ссылке, которая откроется вкладка
+    document.getElementById(cityName).style.display = "flex";
+    evt.currentTarget.className += " active";
+  }
